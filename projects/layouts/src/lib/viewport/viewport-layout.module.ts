721b967df0482a8b3cb7cb48bc5e99e5447f03b9
achugaev <achugaev93@gmail.com>
2019-02-04 00:27:15 +0200

    example improvements

--- projects/layouts/src/lib/viewport/viewport-layout.module.ts
+++ projects/layouts/src/lib/viewport/viewport-layout.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ViewportComponent } from './viewport/viewport.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ViewportComponent} from './viewport/viewport.component';
 
 @NgModule({
   declarations: [ViewportComponent],
@@ -9,4 +9,5 @@ import { ViewportComponent } from './viewport/viewport.component';
     CommonModule
   ]
 })
-export class ViewportLayoutModule { }
+export class ViewportLayoutModule {
+}
