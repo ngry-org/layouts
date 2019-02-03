cf1f35a95e408d0273ba814c1b5f3a56cfd9afc8
achugaev <achugaev93@gmail.com>
2019-02-03 19:40:56 +0200

    attribute-base styles for component states

--- projects/layouts/src/lib/panel/panel/panel.component.ts
+++ projects/layouts/src/lib/panel/panel/panel.component.ts
@@ -7,38 +7,10 @@ import {Component, HostBinding, Input} from '@angular/core';
 })
 export class PanelComponent {
   @Input()
+  @HostBinding('attr.x')
   x: 'left' | 'center' | 'right' = 'center';
 
   @Input()
+  @HostBinding('attr.y')
   y: 'top' | 'center' | 'bottom' = 'center';
-
-  @HostBinding('class.x-left')
-  get xLeft(): boolean {
-    return this.x === 'left';
-  }
-
-  @HostBinding('class.x-center')
-  get xCenter(): boolean {
-    return this.x === 'center';
-  }
-
-  @HostBinding('class.x-right')
-  get xRight(): boolean {
-    return this.x === 'right';
-  }
-
-  @HostBinding('class.y-top')
-  get yTop(): boolean {
-    return this.y === 'top';
-  }
-
-  @HostBinding('class.y-center')
-  get yCenter(): boolean {
-    return this.y === 'center';
-  }
-
-  @HostBinding('class.y-bottom')
-  get yBottom(): boolean {
-    return this.y === 'bottom';
-  }
 }
--- projects/layouts/src/lib/stack/stack/stack.component.ts
+++ projects/layouts/src/lib/stack/stack/stack.component.ts
@@ -7,6 +7,7 @@ import {Component, HostBinding, Input} from '@angular/core';
 })
 export class StackComponent {
   @Input()
+  @HostBinding('attr.direction')
   direction: 'column' | 'row' = 'column';
 
   @HostBinding('class.row')
